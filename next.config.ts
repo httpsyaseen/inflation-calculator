e6ccf191e4bc58155bb91ba1bd3a8c4d8afdfd54
httpsyaseen <httpsyaseen@gmail.com>
2025-06-05 00:04:40 +0500

    hehe

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off", // turn off the rule
-    // or to warn instead of error:
-    // "@typescript-eslint/no-explicit-any": "warn",
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
